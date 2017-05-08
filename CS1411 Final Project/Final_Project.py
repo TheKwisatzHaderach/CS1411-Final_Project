b076bcf69ac34e9d94d438d84551e75873ee1401
Sterling Collins <scstercollins@gmail.com>
2017-05-08 10:59:37 -0500

    Update Final_Project.py

--- CS1411 Final Project/Final_Project.py
+++ CS1411 Final Project/Final_Project.py
@@ -1,28 +1,41 @@
+#makeChange()
+#requires a register.txt file with amount of "Twentys:", "Tens :","Fives:","Ones:","Quarters:", "Dimes:", "Nickels:", "Pennies:
+#file should look like 5 10 15 20 20 20 20 20 which means the register has 5 twentys, 10 tens, 15 fives, 20 ones, 20 quarters etc...
+#register.txt example: 5 10 15 20 20 20 20 20
+#Inputs: total cost and Cash given by user
+#Outputs: False if register.txt isnt found
+#False if cash < total
+#False if not enough change in register
+#True with change values printed also updates register with new totals
 def makeChange(total, cash):
+    total = int(total*100)#to avoid float innacuracy problems
+    cash = int(cash*100) #to avoid float innacuracy problems
     money = 0
     changeValues = []
-    names = ["Twentys:", "Tens :","Fives:","Ones:","Quarters:", "Dimes:", "Nickels:", "Pennies:"]
-    values = [20.00,10.00,5.00,1.00,0.25,0.10,0.05,0.01]
-    if(cash < total):
+    names = ["Twentys:", "Tens :","Fives:","Ones:","Quarters:", "Dimes:", "Nickels:", "Pennies:"] #list representing registers
+    values = [2000,1000,500,100,25,10,5,1]
+    if(cash < total): #makes sure enough cash was given
         print("You did not give enough cash, please give", total, "dollars")
         return False
-    try:
+    try:#exception handling to make sure register file exist
         with open("register.txt") as f:
             for line in f:
                 line = line.split() # to deal with blank 
             if (line):            # lines (ie skip them)
                 registerValues = [int(i) for i in line]
     except FileNotFoundError:
-        print("Your register is not open, please exit the program and add register.txt file to your path with the cash couts inside")
+        print("Your register is not open, please exit the program and open the register by creating a register.txt file with the cash couts inside")
         return False
     change = cash - total
     totalChange = cash - total
-    for i in range(0,8):
-        if(change >= values[i]):
+    for i in range(0,8): # calculates change
+        if(change >= values[i]): #marks change value
             temp = int(change//values[i])
             if(temp > registerValues[i]):
                 changeValues.append(registerValues[i])
-                change = change = change - (registerValues[i]*values[i])
+                print(change,"1")
+                change = change - (registerValues[i]*values[i])
+                print(change,"2")
                 registerValues[i] = 0
             else: 
                 changeValues.append(temp)
@@ -30,20 +43,20 @@ def makeChange(total, cash):
                 registerValues[i] = registerValues[i] - temp
         else:
             changeValues.append(0)
-    print(change)
     if(change > 0):
         print("This register does not have enough change, please give a lower cash value so that we can complete your order. ")
         return False
-    for x in range(0,8):
+    for x in range(0,8): # prints change exact values
         print(names[x], changeValues[x])
-    with open("register.txt", "w") as w:
+    with open("register.txt", "w") as w: #updates register
         for x in registerValues:
             w.write(str(x))
             w.write(" ")
-    print("Total Change:",totalChange)
+    print("Total Change:",(totalChange/100))
     return True
 
-
+#findItem() passes in the user inputted item and a items dictionary with the corresponding prices
+#the function checks if the item exist and return corresponding item price as float if it does and return False if item dosent exist
 def findItem(item,iDict): # pass in item from user and inventory dictionary
     #checks if item exist
     try:
@@ -52,6 +65,7 @@ def findItem(item,iDict): # pass in item from user and inventory dictionary
         return False #if item is not found  
 
 #main passes in a dictionary with the items as the key and price as the values
+#has exception handling for user inputs as floats
 def main(itemDict):
     import time #imports time module
     total = 0.00
@@ -81,39 +95,24 @@ def main(itemDict):
                 total = total + price
                 
     print("Your total is:",total)
-    while(True):
-        cash = (input("Please enter the cash amount you would like to pay with as a float: ")) #Taking user payment
+    while(True): #exception handling looking for float and exit
+        cash = input("Enter how much cash you want to pay with: ")
+        if(cash.lower() == "exit"):
+            return False
         try:
-            cash = cash.lower() 
-            if(cash == "exit"):
-                return False
-            elif(item == "done"):
-                break
-            else:
-                print("Not a valid input")
-        except AttributeError: #Haappens if user enters number
-            pass
-        
-    while(True): #checks if its a float
-        try:
-            cashf = float(cash)
-            checker = makeChange(total,cashf)
-            if (checker == False):
-                pass
-            else:
-                break
+            cash = float(cash)
+            makeChange(total,cash)
+            break
         except ValueError:
-            print("Please make sure you enter a float")
+            print("Please enter a float")
+                           
             
     print("Thanks for coming!")
-    check = input("If you would like to go again enter anything, if you would like to exit enter exit: ")
     print(" ")
     print(" ")
-    if(check.lower() == "exit"):
-        return False
-    else:
-        return True
+    return False
 
+#main run of the program
 import time #imports time module
 import csv #imports csv module to read csv files easier
 itemsList = [] #empty list 
@@ -121,7 +120,6 @@ with open('inventory.csv','r') as f: #Opens file before while loop so that file 
     inventoryDict = {}#empty dictionary for items
     reader = csv.reader(f) #csv.reader reads csv file csv is module .reader() is function
     for row in reader: #reads each row as list
-        print(row)
         k,v = row #k is the items and v is the price k,v are strings
         itemString = ' '.join(map(str, row)) #takes list an converts to 1 string for easier console printing
         itemsList.append(itemString) #adds to item list
@@ -139,4 +137,7 @@ while(True): #start of User program
     else:
         print("Session Exited...")
         time.sleep(3)
+        print("\n")
+        print("\n")
+        print("\n")
 
